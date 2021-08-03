c280a11724c22ce0bd837d3e91386c89812edd97
Alex Rae <arae@spartaglobal.com>
2021-08-03 13:55:48 +0100

    Finished Abstraction

---
+++ OOPPrinciples/OOPPrinciples/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Customer
+{
+    public string FirstName;
+    public string LastName;
+
+    public Customer(string FName, string LName)
+    {
+        FirstName = FName;
+        LastName = LName;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Full Name: {FirstName} {LastName}");
+    }
+}
+
---
+++ OOPPrinciples/OOPPrinciples/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OOPPrinciples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer cust = new Customer("Alex", "Rae");
+            cust.Print();
+        }
+    }
+}
